8b77542c796b473752b64c914273a2efc3940047
KushanDB <164739924+KushanDB@users.noreply.github.com>
2025-11-19 11:08:25 +0530

    Remove react-toastify from App component
    Eliminated the import and usage of ToastContainer and its CSS from App.jsx, cleaning up unused notification code.

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,10 @@ import Education from "./components/Education";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
     <div className="app">
-      <ToastContainer />
       <Navbar />
       <main>
         <section id="home">
